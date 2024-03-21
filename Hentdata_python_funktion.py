fd1f3f43c817027abd3157c910566863be291cf7
Emil Thranholm <ethranholm@hotmail.com>
2024-03-21 20:55:56 +0100

    hent data python_script

---
+++ Hentdata_python_funktion.py
@@ -0,0 +1,76 @@
+def hent_data(xml_url, geo_kreds = "Storkredse", stemmer = "parti"):
+  
+  geo_kreds = geo_kreds.lower().capitalize()
+  stemmer = stemmer.lower().capitalize()
+  
+  ## Tjekker at værdier er gyldige
+  if stemmer not in ["Parti", "Person"]:
+    raise TypeError("Stemmer skal være enten 'Parti' eller 'Person'.")
+  
+  if geo_kreds not in ["Land", "Landsdele", "Storkredse", "Opstillingskredse", "Afstemningsomraader"]:
+    raise TypeError("Der er angivet ugyldig geo_kreds, det skal være en af: 'Land', 'Landsdele', 'Storkredse', 'Opstillingskredse', 'Afstemningsomraader'.")
+  
+  if stemmer == "Person" and geo_kreds not in ["Storkredse", "Opstillingskredse", "Afstemningsomraader"]:
+    raise TypeError("Der kan kun hentes personlige stemmer for geo_kreds: 'Storkredse', 'Opstillingskredse' eller 'Afstemningsomraader'.")
+  
+  ## Henter xml
+  response = requests.get(xml_url)
+  data = xmltodict.parse(response.content)
+  
+  ## Geo_kreds muligheder: Landsdele, Storkredse, Opstillingskredse, Afstemningsomraader, Land
+  temp_geo_kreds = geo_kreds[:-1] if geo_kreds != "Land" else ""
+  
+  ## TODO: Virker ikke for land
+  #geo_xml = data["Data"][geo_kreds][temp_geo_kreds]
+  geo_xml = data["Data"][geo_kreds] if geo_kreds == "Land" else data["Data"][geo_kreds][temp_geo_kreds]
+  
+  xml = [item["@filnavn"] for item in geo_xml] if geo_kreds != "Land" else geo_xml["@filnavn"]
+  
+  ## Laver xml til dict
+  resp = list(map(requests.get, xml)) if geo_kreds != "Land" else requests.get(xml)
+  xml_dict = [xmltodict.parse(resp[item].content) for item in range(len(resp))] if geo_kreds != "Land" else xmltodict.parse(resp.content)
+
+  if (stemmer == "Person"): 
+    temp_stemmer = "Personer" 
+  elif (stemmer == "Parti"): 
+    temp_stemmer = "Stemmer"
+  else:
+    raise TypeError("Ikke gyldig værdi i stemmer, skal være 'Parti' eller 'Person'")
+  exit
+
+  if geo_kreds != "Land":
+    data_liste = [pd.Series(xml_dict[item]["Data"]["Sted"]).to_frame().transpose().join(pd.json_normalize(pd.Series(xml_dict[item]["Data"][temp_stemmer]["Parti"])), rsuffix = "Parti", how = "cross") for item in range(len(xml_dict))]
+  else:
+    data_liste = [pd.Series(xml_dict["Data"]["Sted"]).to_frame().transpose().join(pd.json_normalize(pd.Series(xml_dict["Data"][temp_stemmer]["Parti"])), rsuffix = "Parti", how = "cross")]
+  # pd.concat(data_liste)
+
+  if stemmer == "Parti":
+    data_stemmer = pd.concat(data_liste)
+  elif stemmer == "Person":
+    data_frame = pd.concat(data_liste).explode("Person", ignore_index = True)
+    data_stemmer = data_frame.join(pd.json_normalize(data_frame["Person"]), lsuffix = "Parti")
+  exit
+  
+  data_stemmer.columns = data_stemmer.columns.str.replace("#|@|\\.", "", regex = True)
+
+  if stemmer == "Person":
+  ## Ændrer for dem på Bornholm, der kommer sjovt ud af json_normalise
+    data_stemmer["Navn"] = data_stemmer["Navn"].fillna(data_stemmer["PersonNavn"])
+    
+    data_stemmer["PersonligeStemmer"] = data_stemmer["PersonligeStemmer"].fillna(data_stemmer["PersonPersonligeStemmer"])
+    data_stemmer["TildeltePartiStemmer"] = data_stemmer["TildeltePartiStemmer"].fillna(data_stemmer["PersonTildeltePartiStemmer"])
+    data_stemmer["StemmerIAlt"] = data_stemmer["StemmerIAlt"].fillna(data_stemmer["PersonStemmerIAlt"])
+    
+    ## Kandidater udenfor partierne
+    data_stemmer["Navn"] = data_stemmer["Navn"].fillna(data_stemmer["navn"])
+    
+    data_stemmer["PersonligeStemmer"] = data_stemmer["PersonligeStemmer"].fillna(data_stemmer["PersonligeStemmerParti"])
+    data_stemmer["TildeltePartiStemmer"] = data_stemmer["TildeltePartiStemmer"].fillna(data_stemmer["TildeltePartiStemmerParti"])
+    data_stemmer["StemmerIAlt"] = data_stemmer["StemmerIAlt"].fillna(data_stemmer["StemmerIAltParti"])
+  exit
+
+  # Fjerner variable
+  data_stemmer.drop(["Person", "PersonNavn", "PersonPersonligeStemmer", "PersonTildeltePartiStemmer", "PersonStemmerIAlt"], axis = 1, inplace = True, errors = "ignore")
+
+  data_stemmer.rename(columns={"text": "Storkreds", "navn": "Parti", "StemmerIAlt": "PersonligeStemmerIAlt"}, inplace = True, errors = "ignore")
+  return(data_stemmer)
---
+++ import_valgdata.py
@@ -0,0 +1,20 @@
+## Henter xml-data med Python
+
+import pandas as pd
+import numpy as np
+import requests
+import xmltodict
+
+# Læser funktion
+exec(open("Hentdata_python_funktion.py").read())
+
+valg_url = "https://www.dst.dk/valg/Valg1968094/xml/fintal.xml"
+landsdele = hent_data(xml_url = valg_url, geo_kreds = "Landsdele", stemmer = "Parti")
+storkredse_parti = hent_data(xml_url = valg_url, geo_kreds = "storkredse", stemmer = "parti")
+storkredse_person = hent_data(xml_url = valg_url, geo_kreds = "storkredse", stemmer = "person")
+
+landsdele
+storkredse_parti
+storkredse_person
+
+
